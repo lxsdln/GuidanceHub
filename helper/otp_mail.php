38458083831104f40abf4ba2c076537ebf8ec13e
lxsdln <lex04deleon@gmail.com>
2025-09-09 22:45:02 -0600

    Create otp_mail.php

---
+++ helper/otp_mail.php
@@ -0,0 +1,33 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require __DIR__ . '/../vendor/autoload.php';
+
+
+function sendOTPEmail($to, $otp) {
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'deleon04alexis@gmail.com'; // your SMTP username
+        $mail->Password   = 'qnry mkhs buqg nhdx'; // your SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
+
+        $mail->setFrom('deleon04alexis@gmail.com', 'Guidance Office');
+        $mail->addAddress($to);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Your OTP Verification Code';
+        $mail->Body = "<p>Your OTP code is: <b>$otp</b></p><p>This code expires in 5 minutes.</p>";
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        error_log("Mailer Error: {$mail->ErrorInfo}");
+        return false;
+    }
+}
+?>
